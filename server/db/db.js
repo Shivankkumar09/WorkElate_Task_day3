df1b4f3a4f20d0f2c35fbca9b9ef8d3af75090b5
Shivank <115658818+Shivankkumar09@users.noreply.github.com>
2025-07-24 23:10:34 +0530

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI  || "mongodb://localhost:27017/task", {
+    await mongoose.connect(process.env.MONGODB_URI  || "mongodb://localhost:27017/form", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
